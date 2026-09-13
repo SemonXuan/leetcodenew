9f0f7fefcb5a3ba8b45d60bb5a0bf2fa5fec1c01
SunnyWood <10193599+SemonXuan@users.noreply.github.com>
2026-09-13 21:38:20 +0800

    add two new ways

--- leetcode2026/1768_merge_string_alternately.py
+++ leetcode2026/1768_merge_string_alternately.py
@@ -1,8 +1,15 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        merged = ""
+        merged = []
         leni = min(len(word1),len(word2))
         for i in range(leni):
-            merged += word1[i] + word2[i]
-        merged += word1[leni:] + word2[leni:]
-        return merged
+            merged.append(word1[i] + word2[i])
+        merged.append(word1[leni:] + word2[leni:])
+        return ''.join(merged)
+
+# 方法二：
+# merged = [i+j for i,j in zip(test1, test2)]
+# print(''.join(merged) + test1[len(merged):] + test2[len(merged):])
+
+# 方法三：
+# print(''.join([i+j for i,j in zip_longest(test1, test2, fillvalue='')]))
