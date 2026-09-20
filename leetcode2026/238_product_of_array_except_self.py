bb95a81edb4f459bb93186e2adf5becb85f36b9f
SunnyWood <10193599+SemonXuan@users.noreply.github.com>
2026-09-20 21:33:17 +0800

    change 238

--- leetcode2026/238_product_of_array_except_self.py
+++ leetcode2026/238_product_of_array_except_self.py
@@ -12,3 +12,19 @@ class Solution:
                 cnums.remove(i)
                 rnums.append(math.prod(cnums))
         return rnums
+    
+# class Solution:
+#     def productExceptSelf(self, nums: list[int]) -> list[int]:
+#         returnnums = [1]
+#         rnums = 1
+#         i = 1
+#         while i < len(nums):
+#             returnnums.append(returnnums[i-1] * nums[i-1])
+#             i += 1
+#         j = len(nums) - 2
+#         while j >= 0:
+#             rnums *= nums[j+1]
+#             returnnums[j] = returnnums[j] * rnums
+#             j -= 1
+#         return returnnums
+        
